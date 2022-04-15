fe6d1028570fafb34e85f2c31f9018db854a9012
Jonathan <creaw15@gmail.com>
2022-04-15 09:33:15 -0400

    update(table): add generic props to re-use table.

--- src/app/components/table/table.component.ts
+++ src/app/components/table/table.component.ts
@@ -1,11 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Column, Row } from 'src/app/models/components/table-models';
 
 @Component({
   selector: 'app-table',
   templateUrl: './table.component.html',
   styleUrls: ['./table.component.scss']
 })
-export class TableComponent implements OnInit {
+export class TableComponent<T> implements OnInit {
+  @Input() columns: Column<T>[] = [];
+  @Input() rows: Row<T>[] = [];
 
   constructor() { }
 
