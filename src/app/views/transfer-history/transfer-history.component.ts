ef6ca4824c3b2b021687e396a4c2720a2efe0e11
Jonathan <creaw15@gmail.com>
2022-04-15 09:50:30 -0400

    update(transfer-history): add dummy data.

--- src/app/views/transfer-history/transfer-history.component.ts
+++ src/app/views/transfer-history/transfer-history.component.ts
@@ -1,11 +1,29 @@
 import { Component, OnInit } from '@angular/core';
 
+interface Car {
+  manufacturer: string;
+  model: string;
+  powerSupply: "petrol" | "diesel" | "electricity";
+};
 @Component({
   selector: 'app-transfer-history',
   templateUrl: './transfer-history.component.html',
   styleUrls: ['./transfer-history.component.scss']
 })
 export class TransferHistoryComponent implements OnInit {
+  public displayedColumns = ["manufacturer", "model", "powerSupply"];
+  public dataSource: Car[] = [
+    {
+      manufacturer: "Tesla",
+      model: "Model 3",
+      powerSupply: "electricity",
+    },
+    {
+      manufacturer: "Ferrari",
+      model: "458",
+      powerSupply: "petrol",
+    },
+  ];
 
   constructor() { }
 
