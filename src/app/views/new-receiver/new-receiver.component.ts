b24528f1ad7d509647cd19134ddc5c600737fcb7
Jonathan <creaw15@gmail.com>
2022-04-15 04:07:18 -0400

    update(new-receiver): added form logic and accountType const.

--- src/app/views/new-receiver/new-receiver.component.ts
+++ src/app/views/new-receiver/new-receiver.component.ts
@@ -1,15 +1,36 @@
+import { FormBuilder, Validators } from '@angular/forms';
 import { Component, OnInit } from '@angular/core';
+
+import { AccountType } from 'src/app/models/account-model';
+import { ACCOUNT_TYPES_FACTORY } from 'src/app/constants/account-types';
+import { Receiver, ReceiverFormControl } from 'src/app/models/receiver-model';
 
 @Component({
   selector: 'app-new-receiver',
   templateUrl: './new-receiver.component.html',
   styleUrls: ['./new-receiver.component.scss']
 })
+
 export class NewReceiverComponent implements OnInit {
+  accountTypes: AccountType[]  = ACCOUNT_TYPES_FACTORY;
 
-  constructor() { }
+  newReceiverForm: ReceiverFormControl =
+    this.formBuilder.group({
+      phone: [null],
+      rut: ['', [Validators.required]],
+      name: ['', [Validators.required]],
+      bank: [null, [Validators.required]],
+      accountType: [null, [Validators.required]],
+      accountNumber: [null, [Validators.required]],
+      email: ['', [Validators.required, Validators.email]]
+    }) as ReceiverFormControl;
 
-  ngOnInit(): void {
-  }
+  constructor(private formBuilder: FormBuilder) { };
 
-}
+  ngOnInit(): void { };
+
+  saveReceiver(): void { 
+    console.log(this.newReceiverForm.value);
+  };
+
+};
