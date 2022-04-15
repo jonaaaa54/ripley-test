987c830d8685ed5200b31108bdf0db1408343c88
Jonathan <creaw15@gmail.com>
2022-04-14 21:47:39 -0400

    add(new-receiver): add new component view.

---
+++ src/app/views/new-receiver/new-receiver.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-new-receiver',
+  templateUrl: './new-receiver.component.html',
+  styleUrls: ['./new-receiver.component.scss']
+})
+export class NewReceiverComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
