5bfe0336747fa21a1d73b9229b629fa386d06e4c
Jonathan <creaw15@gmail.com>
2022-04-14 21:25:51 -0400

    added:(navigation-bar): new nav-bar component.

--- src/app/views/navigation-bar/navigation-bar.component.ts
+++ src/app/views/navigation-bar/navigation-bar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-navigation-bar',
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navigation-bar.component.scss']
 })
 export class NavigationBarComponent implements OnInit {
+  @Input() actualComponent: string = '';
 
   constructor() { }
 
