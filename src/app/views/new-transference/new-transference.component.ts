73de8e3f12ffa8258f2addb485c1da2e99450623
Jonathan <creaw15@gmail.com>
2022-04-15 07:55:10 -0400

    update(new-transference): added custom form valid, and form control.

--- src/app/views/new-transference/new-transference.component.ts
+++ src/app/views/new-transference/new-transference.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, Validators } from '@angular/forms';
+
+import { Receiver, TransferToReceiver, TransferToReceiverFormControl } from 'src/app/models/receiver-model';
 
 @Component({
   selector: 'app-new-transference',
@@ -6,10 +9,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./new-transference.component.scss']
 })
 export class NewTransferenceComponent implements OnInit {
+  receiver: Receiver | undefined;
 
-  constructor() { }
+  receiverTransferForm: TransferToReceiverFormControl = 
+    this.formBuilder.group({
+      amount: [null, [Validators.required, this.validAmount]],
+      receiver: [null, [Validators.required]]
+    })as TransferToReceiverFormControl;
+  
+  constructor(private formBuilder: FormBuilder) {
+  }
 
   ngOnInit(): void {
   }
 
+  transfer() {
+    console.log(this.receiverTransferForm.value);
+  };
+
+  private validAmount(amount: FormControl) {
+    return amount.value > 0 ? null : {
+      valid: true
+    };
+  }
 }
