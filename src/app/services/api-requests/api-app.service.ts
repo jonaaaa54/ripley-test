447446a87b0912eeca190fbed35c86755ef8a03c
Jonathan <creaw15@gmail.com>
2022-04-18 12:33:18 -0400

    add(api-app): added new service to api-app.

--- src/app/services/api-requests/api-app.service.ts
+++ src/app/services/api-requests/api-app.service.ts
@@ -1,9 +1,28 @@
 import { Injectable } from '@angular/core';
 
+import { Observable } from 'rxjs';
+
+import { ApiFactory } from 'src/app/config/api-config';
+import { Receiver } from 'src/app/models/receiver-model';
+import { ResponseBase } from 'src/app/models/response-base';
+import { HttpRequestService } from '../http-request.service';
+
 @Injectable({
   providedIn: 'root'
 })
 export class ApiAppService {
+  
+  constructor(
+    private apiUrls: ApiFactory,
+    private httpService: HttpRequestService
+  ) {};
 
-  constructor() { }
+  addReceiver(data: Receiver): Observable<ResponseBase<Receiver>> {
+      return this.httpService.post(this.apiUrls.API_APP + '/receiver', data);
+  };
+
+  getReceiverByRut(rut: string): Observable<ResponseBase<Receiver>> {
+    return this.httpService.get(this.apiUrls.API_APP + '/transference/' + rut);
+  };
+
 }
