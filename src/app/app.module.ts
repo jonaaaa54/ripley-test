6a67997247040b46e5bbb81b3979f18922fbf2a5
Jonathan <creaw15@gmail.com>
2022-04-18 06:58:47 -0400

    update(app.module): import toast component and import that module.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations'; 
 
+import { ToastModule } from 'primeng/toast';
 import { TableModule } from 'primeng/table';
 import { ButtonModule } from 'primeng/button';
 import { DropdownModule } from 'primeng/dropdown';
@@ -16,6 +17,7 @@ import { AppComponent } from './app.component';
 import { ApiFactory } from './config/api-config';
 import { AppRoutingModule } from './app-routing.module';
 import { TableComponent } from './components/table/table.component';
+import { ToastComponent } from './components/toast/toast.component';
 import { NewReceiverComponent } from './views/new-receiver/new-receiver.component';
 import { NavigationBarComponent } from './views/navigation-bar/navigation-bar.component';
 import { NewTransferenceComponent } from './views/new-transference/new-transference.component';
@@ -28,12 +30,14 @@ import { TransferHistoryComponent } from './views/transfer-history/transfer-hist
     NewReceiverComponent,
     NavigationBarComponent,
     NewTransferenceComponent,
-    TransferHistoryComponent
+    TransferHistoryComponent,
+    ToastComponent
   ],
   imports: [
     NgbModule,
     FormsModule,
     TableModule,
+    ToastModule,
     ButtonModule,
     BrowserModule,
     DropdownModule,
