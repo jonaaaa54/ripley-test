9383f1991f5680cfa30c7fcd20839e66207643c3
Jonathan <creaw15@gmail.com>
2022-04-18 03:34:39 -0400

    update(bank-model): added new banks collection model.

--- src/app/models/banks-model.ts
+++ src/app/models/banks-model.ts
@@ -1,6 +1,11 @@
-export interface Banks{
+export interface Bank {
   /** Name of the bank. */
   name: string;
   /** Id of the bank. */
   id: string;
+};
+
+export interface Banks {
+  /** List of banks. */
+  banks: Bank[];
 };
