8a4e6dd2d15b6df4b8488bf034c6336131ec7c95
Jonathan <creaw15@gmail.com>
2022-04-18 06:36:30 -0400

    add(error-catching): new interceptor to all api request.

--- src/app/interceptors/error-catching.interceptor.ts
+++ src/app/interceptors/error-catching.interceptor.ts
@@ -1,18 +1,35 @@
-import { Injectable } from '@angular/core';
 import {
+  HttpEvent,
   HttpRequest,
   HttpHandler,
-  HttpEvent,
-  HttpInterceptor
+  HttpInterceptor,
+  HttpErrorResponse
 } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Injectable } from '@angular/core';
+
+import { catchError, map, Observable, throwError } from 'rxjs';
 
 @Injectable()
 export class ErrorCatchingInterceptor implements HttpInterceptor {
 
-  constructor() {}
+  constructor() {
+  };
 
-  intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
-    return next.handle(request);
-  }
-}
+  intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    return next.handle(request)
+      .pipe(
+        map(res => res),
+        catchError((error: HttpErrorResponse) => {
+          let errorMsg = '';
+          if (error.error instanceof ErrorEvent) {
+            console.log('This is client side error');
+            errorMsg = `Error: ${error.error.message}`;
+          } else {
+            console.log('This is server side error');
+            errorMsg = `Error Code: ${error.status},  Message: ${error.message}`;
+          }
+          console.log(errorMsg);
+          return throwError(() => errorMsg);
+        }))
+  };
+};
