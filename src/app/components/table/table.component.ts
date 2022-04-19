eb0d61c1e47fc098baab5df99ffa9b626c4f8b5d
Jonathan <creaw15@gmail.com>
2022-04-19 03:54:00 -0400

    update(table): added logic.

--- src/app/components/table/table.component.ts
+++ src/app/components/table/table.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { Router } from '@angular/router';
+import { Observable } from 'rxjs';
 import { Column, Row } from 'src/app/models/components/table-models';
 
 @Component({
@@ -7,12 +9,27 @@ import { Column, Row } from 'src/app/models/components/table-models';
   styleUrls: ['./table.component.scss']
 })
 export class TableComponent<T> implements OnInit {
-  @Input() columns: Column<T>[] = [];
-  @Input() rows: Row<T>[] = [];
+  @Input() rows: Row<T>[] | undefined;
+  @Input() getColumns?: boolean = false; 
+  @Input() columns?: Column<T>[] = [];
 
-  constructor() { }
+  localColumns: string[] = [];
 
-  ngOnInit(): void {
+  constructor(private router: Router) { 
+    if(this.getColumns) this.localColumns = this.getColumnsFromObject();
   }
 
-}
+  ngOnInit(): void {
+    if(this.getColumns) this.localColumns = this.getColumnsFromObject(); 
+  }
+
+  private getColumnsFromObject(): string[] {
+    if(this.rows)
+      return Object.keys(this.rows)
+        .map( keys =>
+          (keys.replace(/[A-Z]/g, ' $&').trim()).toLowerCase()
+        );
+    return [];
+  };
+
+};
