431c1ef83f34b771b3c829f1ce7521dc3931af27
Jonathan <creaw15@gmail.com>
2022-04-19 04:00:43 -0400

    add(table-headers): new const file.

---
+++ src/app/constants/components/table-headers.ts
@@ -0,0 +1,7 @@
+export const TABLE_DEFAULT_HEADERS = [
+  {label: 'Nombre destinatario', key: 'name'},
+  {label: 'Rut', key: 'rut'},
+  {label: 'Banco', key: 'bank'},
+  {label: 'Tipo de cuenta', key: 'accountType'},
+  {label: 'Monto', key: 'amount'}
+];
